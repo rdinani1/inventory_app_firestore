84b19d93e093acb416df0f25ff8461ab0c61abc7
rdinani1 <rdinani1@student.gsu.edu>
2026-04-06 18:22:11 -0400

    add item model with toMap and fromMap

---
+++ lib/models/item.dart
@@ -0,0 +1,30 @@
+class Item {
+  final String? id;
+  final String name;
+  final int quantity;
+  final double price;
+
+  Item({
+    this.id,
+    required this.name,
+    required this.quantity,
+    required this.price,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'price': price,
+    };
+  }
+
+  factory Item.fromMap(String id, Map<String, dynamic> map) {
+    return Item(
+      id: id,
+      name: map['name'] ?? '',
+      quantity: (map['quantity'] ?? 0) as int,
+      price: (map['price'] ?? 0).toDouble(),
+    );
+  }
+}
